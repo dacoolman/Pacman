79245de1090ba694ca0d92456bc0c5eb11e2069d
Michael Berger <mikeberger.riceowl@gmail.com>
2016-06-21 03:09:14 -0700

    Adding level

--- js/levels.js
+++ js/levels.js
@@ -93,3 +93,17 @@ Game.levels[4] = {
   	ghosts: [new Ghost(1,1,'ghost'), new Ghost(2,2,'redghost'), new Ghost(2,8,'redghost'), new Ghost(8,8,'redghost'), new Ghost(7,8,'ghost')]
 };
 
+Game.levels[5] = {
+	world: [0, 3, 3, 3, 0, 0, 2, 0, 0, 0, 0, 3, 3, 3, 2, 1, 1, 0, 2, 0, 0, 3, 0, 3, 2, 1, 1, 0, 1, 0, 0, 3, 3, 3, 2, 1, 1, 0, 1, 0, 2, 3, 0, 3, 3, 1, 1, 0, 1, 2, 0, 2, 3, 3, 2, 1, 1, 0, 1, 0, 0, 3, 0, 3, 2, 1, 1, 0, 1, 0, 0, 3, 3, 3, 2, 1, 1, 0, 1, 0, 0, 3, 0, 3, 3, 1, 1, 1, 1, 0, 0, 3, 3, 3, 0, 0, 2, 0, 0, 0],
+	pacman: new Pacman(1,5,'pacman', {left: 'left', up: 'up', right: 'right', down: 'down'}),
+	ghosts: [
+		new Ghost(4,1,'ghost'),
+		new Ghost(4,2,'ghost'),
+		new Ghost(4,3,'ghost'),
+		new Ghost(4,5,'ghost'),
+		new Ghost(4,6,'ghost'),
+		new Ghost(4,7,'ghost'),
+		new Ghost(8,1,'redghost'),
+	],
+};
+
